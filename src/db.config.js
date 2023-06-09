39db6e4faa0c059565ecd83637b86bdd25cba3fc
Abid Rafi Nur Hastama <abidrafinurhastama@gmail.com>
2023-06-09 15:51:30 +0700

    change port

--- src/db.config.js
+++ src/db.config.js
@@ -5,7 +5,8 @@ const connection = mysql.createConnection({
   MYSQLUSER: 'root',
   MYSQLPASSWORD: 'YX2gWv2bVXnAtDGCjQrq',
   MYSQLDATABASE: 'railway',
-  MYSQLPORT: 5880
+  MYSQLPORT: 5880,
+  MYSQL_URL: 'mysql://root:YX2gWv2bVXnAtDGCjQrq@containers-us-west-107.railway.app:5880/railway'
 })
 
 connection.connect((error) => {
--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 3000,
+    port: process.env.PORT || 8080,
     host: '0.0.0.0',
     routes: {
       cors: {
