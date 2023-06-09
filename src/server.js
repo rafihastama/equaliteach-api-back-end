e3fd98f8fec47879bd31ed4304d6387adb5db142
Abid Rafi Nur Hastama <abidrafinurhastama@gmail.com>
2023-06-09 16:53:06 +0700

    change port

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT || 8080,
+    port: process.env.PORT || 443,
     host: '0.0.0.0',
     routes: {
       cors: {
