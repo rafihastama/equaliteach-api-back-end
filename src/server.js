eb0444afc5a7866da9a72400353e9a1a90422aff
Abid Rafi Nur Hastama <abidrafinurhastama@gmail.com>
2023-06-09 14:20:24 +0700

    fixing variable port issue

--- src/server.js
+++ src/server.js
@@ -3,8 +3,8 @@ const routes = require('./routes')
 
 const init = async () => {
   const server = Hapi.server({
-    MYSQLPORT: process.env.PORT || 3000,
-    MYSQLHOST: 'localhost',
+    PORT: process.env.PORT || 3000,
+    host: 'localhost',
     routes: {
       cors: {
         origin: ['*']
