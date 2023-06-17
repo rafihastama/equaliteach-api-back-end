68ceef9c4120c3af0b232be128fc50cc2a47437f
Abid Rafi Nur Hastama <abidrafinurhastama@gmail.com>
2023-06-17 20:23:22 +0700

    resolving something

--- src/db.config.js
+++ src/db.config.js
@@ -2,7 +2,7 @@ const mysql = require('mysql')
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT || 5580,
+  port: process.env.DB_PORT || 6020,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
